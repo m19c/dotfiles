8a3623d6451d78619e36d4a716e9b0f8a297031d
Marc Binder <marcandrebinder+pub@gmail.com>
2024-06-18 13:35:03 +0200

    feat: setup background image script

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 PRIVATE_ENV_FILE=~/.privateenv
 
-
 if test ! $(which brew); then
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
@@ -26,7 +25,8 @@ stow -t $HOME \
   tmux \
   tmuxinator \
   vscode \
-  zsh
+  zsh \
+  nvim
 
 require_private_env() {
   local prompt=$1
@@ -45,3 +45,13 @@ require_private_env() {
 }
 
 require_private_env "1Password Account ID" "OP_ACCOUNT"
+
+BACKGROUND_IMAGE=future.png
+LOCAL_IMAGE_PATH=$(realpath ./background/${BACKGROUND_IMAGE})
+osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'"$LOCAL_IMAGE_PATH"'"'
+BACKGROUND_RESULT=$?
+if [ $BACKGROUND_RESULT == "0" ]; then
+  echo "wallpaper set successfully"
+else
+  echo "unable to set wallpaper"
+fi
