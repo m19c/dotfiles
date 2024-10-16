049e9126100c4d9c74e4a34c979866f792b8c2c7
Marc Binder <marcandrebinder+pub@gmail.com>
2024-10-16 09:37:28 +0200

    feat: init goenv

--- zsh/.config/zsh/lib/lang/go.zsh
+++ zsh/.config/zsh/lib/lang/go.zsh
@@ -2,3 +2,7 @@ export GOPRIVATE="github.com/digitalegarage"
 export GO111MODULE=on
 export GOBIN=~/Development/go/bin
 export PATH=$PATH:~/Development/go/bin
+
+eval "$(goenv init -)"
+export PATH="$GOROOT/bin:$PATH"
+export PATH="$PATH:$GOPATH/bin"
